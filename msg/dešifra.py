bde8b958c60696cfd7cb2a1d31a4ea4c10e3da78
Samuel Čaniga <108100262+Spucen1@users.noreply.github.com>
2025-01-23 23:43:21 +0100

    Update dešifra.py

--- msg/dešifra.py
+++ msg/dešifra.py
@@ -1,8 +1,1 @@
-sprava = input("Správa: ")
-first = sprava[::2]
-second = sprava[1::2]
-for sym in first:
-    numb = second[0]
-    second = second[1:]
-    sim = ord(sym) - int(numb)
-    print(chr(sim), end="")
+
