8c42491b37abe1117ac4089514df402ac4836d00
Spucen1 <108100262+Spucen1@users.noreply.github.com>
2025-02-12 16:00:13 +0100

    a

--- personal/jsTest/jsTestV1.js
+++ personal/jsTest/jsTestV1.js
@@ -1,4 +1,3 @@
-console.log("Výpočet obsahu obdĺžnika");
 let button = document.getElementById("button");
 button.addEventListener("click", function () {
   let a = parseFloat(document.getElementById("a").value);
