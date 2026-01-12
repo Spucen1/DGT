3df98662d9da4f65b73019ebd0e099ed386c493b
Spucen1 <108100262+Spucen1@users.noreply.github.com>
2026-01-12 12:19:02 +0100

    feat: enhance greeting application with user input handling and reset functionality

--- schoolRelated/2026-01-12_zadanie01/main.py
+++ schoolRelated/2026-01-12_zadanie01/main.py
@@ -2,7 +2,7 @@ from browser import document
 
 # TODO: STATUS
 # Skús zmeniť text v prvku 'status' tak, aby si videl, že Brython beží.
-document["status"].text = "Status: TODO (uprav ma v main.py)"
+document["status"].text = "Brython beží!"
 
 # premenná na počítanie pozdravov
 count = 0
@@ -12,8 +12,31 @@ def greet(ev):
     # TODO: načítaj meno z inputu inp_name, ošetri prázdny vstup a vypíš do prvku out
     # Nápoveda: document["inp_name"].value
     # Nápoveda: document["out"].text = "..."
-    pass
+    if ev.type == "keydown" and ev.key != "Enter":
+        return
+    meno = document["inp_name"].value
+    if meno == "":
+        document["out"].text = "Zdaj meno!"
+    else:
+        global count
+        count += 1
+        document["out"].text = f"Ahoj, {meno}! x{count}"
+    # pass
+
+def reset(ev):
+    global count
+    count = 0
+    document["out"].text = ""
+    document["hint"].style.display = "none"
+    document["inp_name"].value = ""
+
+def show_tip(ev):
+    document["hint"].style.display = "block"
+    
 
 # TODO: BIND
 # Pripoj funkciu greet na kliknutie tlačidla btn_greet.
-# Nápoveda: document["btn_greet"].bind("click", greet)
+document["btn_greet"].bind("click", greet)
+document.bind("keydown", greet)
+document["btn_reset"].bind("click", reset)
+document["btn_tip"].bind("click", show_tip)
